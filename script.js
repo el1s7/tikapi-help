998d71345b8ccef353fbf79d18a5f5d4a3dfeca5
Eli$ <me@elis.cc>
2021-08-01 18:25:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ document.addEventListener('DOMContentLoaded', function() {
   var DOWN = 40;
   var TAB = 9;
   
-  if(window.location.pathname.length === 9 && window.location.pathname.substr(0,3) === "/hc"){
+  if(window.location.pathname < 4 || (window.location.pathname.length === 9 && window.location.pathname.substr(0,3) === "/hc")){
     document.body.classList.add("is-home");
+    document.querySelector(".logo img").setAttribute("src","https://tikapi.io/assets/img/ll.png")
   }
   function closest (element, selector) {
     if (Element.prototype.closest) {
